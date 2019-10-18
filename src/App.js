510d72d158ae0b5cdb682eaf5730033a615bce88
Pedro Everett <pedrojoseeverett@gmail.com>
2019-10-18 19:11:12 +0100

    Initial App.js structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PanelWraper = styled('div')`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding-top: 50px;
+`;
+
+const Header = styled('div')`
+  display: table;
+  width: 100%;
+  background-color: grey;
+  color: white;
+  padding-bottom: 20px;
+`;
+
+const AppName = styled('h1')`
+  display: table-cell;
+  text-align: left;
+  padding-left: 50px;
+`;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+  }
+
+  render () {
+    return (
+      <div className="App-main">
+        <Header>
+          <AppName>FanBet - Make Your bets</AppName>
+        </Header>
+        <PanelWraper>
+        </PanelWraper>
+      </div>
+    );
+  }
+
 }
 
 export default App;
