791cf15a579aa4149854924e18282521a35adf7a
Pedro Everett <pedrojoseeverett@gmail.com>
2019-10-19 15:45:35 +0100

    App.Test working

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  const component = shallow(<App />);
+  it('should render correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should contain 1 div element', () => {
+    expect(component.find('div').length).toBe(1);
+  })
+
+  it('should contain main div', () => {
+    expect(component.find('.App-main').exists()).toBe(true);
+  })
 });
